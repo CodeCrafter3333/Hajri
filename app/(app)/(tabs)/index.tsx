af25bee80813c1b088d59f38ff2c17a4b564b54d
Akshay <CodeCrafter3333@gmail.com>
2024-10-16 12:35:30 +0530

    added button

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import { StatusBar } from "expo-status-bar";
 import Header from "@/components/ui/home/Header";
 import Kategori from "@/components/ui/home/Kategori";
 import MealList from "@/components/ui/home/MealList";
+import { TouchableOpacity, Text, View } from "react-native";
 
 const Home = () => {
   const [categoryId, setCategoryId] = useState("");
@@ -13,6 +14,21 @@ const Home = () => {
     <SafeAreaView className="flex-1 bg-white">
       <StatusBar style="dark" />
       <Header />
+      <View className="px-5 my-5">
+      <View className="mb-5 flex-row items-center justify-between">
+        <View>
+
+      <TouchableOpacity
+            className={`bg-blue-600 h-12 rounded-xl items-center justify-center w-full my-5`}
+          >
+              <Text className="text-white font-[poppinsSemiBold] text-base px-5">
+                Create new Orginisation
+              </Text>
+        </TouchableOpacity>
+
+        </View>
+        </View>
+        </View>
       {/* <Kategori categoryId={categoryId} setCategoryId={setCategoryId} />
       <MealList categoryId={categoryId} /> */}
     </SafeAreaView>
